2330ef0969ddbc7c24ba23707a01841da8175be7
Flávio Félix <fau_felix30@hotmail.com>
2024-11-19 14:56:25 -0300

    Criando e estruturando API com GET

--- instalike-back/server.js
+++ instalike-back/server.js
@@ -1,19 +1,30 @@
 import express from "express";
 
+const posts = [
+  { id: 1, descricao: "Uma foto teste", imagem: "https://picsum.photos/200/300"  },
+  { id: 2, descricao: "Gato fofo dormindo", imagem: "https://picsum.photos/id/237/200/300"  },
+  { id: 3, descricao: "Cachorro dormindo", imagem: "https://picsum.photos/id/237/200/300"  },
+];
+
+
 const app = express();
+app.use(express.json());
+
 app.listen(3000, () =>{
   console.log("Servidor escutando...");
 });
 
-app.get("/api", (req, res) =>{
-  res.status(200).send("Boas vindas a imersão!");
+app.get("/posts", (req, res) =>{
+  res.status(200).json(posts);
 });
 
-app.get("/livro", (req, res) =>{
-  res.status(200).send({
-    "titulo": "O Senhor dos Anéis",
-    "autor": "J.R.R. Tolkien",
-    "ano": 1954,
-    "genero": "Fantasia"
-  });
+function getPostById(id) {
+  return posts.findIndex((post) =>{
+    return post.id === Number(id);
+  })
+}
+
+app.get("/posts/:id", (req, res) => {
+  const index = getPostById(req.params.id);
+  res.status(200).json(posts[index]);
 });
