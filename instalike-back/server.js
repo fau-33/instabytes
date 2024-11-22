ee57ca8327c31cb30afd2f068c2ce6b482d2c430
Flávio Félix <fau_felix30@hotmail.com>
2024-11-21 21:17:22 -0300

    Done Refactor

--- instalike-back/server.js
+++ instalike-back/server.js
@@ -1,30 +1,12 @@
 import express from "express";
-
-const posts = [
-  { id: 1, descricao: "Uma foto teste", imagem: "https://picsum.photos/200/300"  },
-  { id: 2, descricao: "Gato fofo dormindo", imagem: "https://picsum.photos/id/237/200/300"  },
-  { id: 3, descricao: "Cachorro dormindo", imagem: "https://picsum.photos/id/237/200/300"  },
-];
-
+import routes from "./src/routes/postsRoutes.js";
 
 const app = express();
+
 app.use(express.json());
 
-app.listen(3000, () =>{
-  console.log("Servidor escutando...");
-});
+routes(app);
 
-app.get("/posts", (req, res) =>{
-  res.status(200).json(posts);
-});
-
-function getPostById(id) {
-  return posts.findIndex((post) =>{
-    return post.id === Number(id);
-  })
-}
-
-app.get("/posts/:id", (req, res) => {
-  const index = getPostById(req.params.id);
-  res.status(200).json(posts[index]);
+app.listen(3000, () => {
+    console.log("Servidor escutando...");
 });
